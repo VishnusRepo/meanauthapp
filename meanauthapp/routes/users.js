483955b15f3b98e33d0a39217f4d4803d06e1b9a
sai vishnu muvvala <saivishnu30@gmail.com>
2019-03-24 19:56:47 -0400

    connected to mongodb

--- meanauthapp/routes/users.js
+++ meanauthapp/routes/users.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 const router = express.Router();
 
-router.get('/register',(req,res,next)=>{
-  res.send('Register');
+router.post('/register',(req,res,next)=>{
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+  User.addUser(newUser,(err,user)=>{
+    if(err){
+      res.json({success:false, msg:'Failed to register user'});
+    }else{
+      res.json({success:true, msg: 'Registered user'});
+    }
+  })
 });
 
 router.post('/authenticate',(req,res,next)=>{
@@ -12,9 +27,9 @@ router.post('/authenticate',(req,res,next)=>{
 router.get('/profile',(req,res,next)=>{
   res.send('Profile');
 })
-
-router.get('/',(req,res,next)=>{
+//Not needed
+/*router.get('/',(req,res,next)=>{
   res.send('Users base page');
-})
+})*/
 
 module.exports=router;
